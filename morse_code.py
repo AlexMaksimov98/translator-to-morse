3c7f083d4666703810c6622f68025d8337b0b6e9
Alex <maksimov4598@mail.ru>
2022-07-04 23:31:12 +0300

    Initial commit

---
+++ morse_code.py
@@ -0,0 +1,38 @@
+morse_alphabet = {
+    'a': '.-',
+    'b': '-...',
+    'c': '-.-.',
+    'd': '-..',
+    'e': '.',
+    'f': '..-.',
+    'g': '--.',
+    'h': '....',
+    'i': '..',
+    'j': '.---',
+    'k': '-.-',
+    'l': '.-..',
+    'm': '--',
+    'n': '-.',
+    'o': '---',
+    'p': '.--.',
+    'q': '--.-',
+    'r': '.-.',
+    's': '...',
+    't': '-',
+    'u': '..-',
+    'v': '...-',
+    'w': '.--',
+    'x': '-..-',
+    'y': '-.--',
+    'z': '--..',
+}
+
+
+def translate_morse(sentence):
+    result = ''
+    for key, value in morse_alphabet.items():
+        for letter in sentence:
+            if letter == key:
+                result += value
+    print(result)
+
